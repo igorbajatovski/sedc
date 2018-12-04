af88d8626dee1d928f07b5e365957f39fa1b2d51
igorbajatovski <igorbajatovski@gmail.com>
2018-12-04 07:18:57 +0100

    Final Commit

---
+++ Homework_cas6/Exercise3/Exercise3.js
@@ -0,0 +1,11 @@
+window.addEventListener("resize", function(event)
+{
+   let innerPageSize = this.document.getElementById("InnerPageSize");
+   innerPageSize.textContent = `Inner Page size is = Width: ${event.target.innerWidth}, Height: ${event.target.innerHeight}`;
+
+   let outerPageSize = this.document.getElementById("OuterPageSize");
+   outerPageSize.textContent = `Outer Page size is = Width: ${event.target.outerWidth}, Height: ${event.target.outerHeight}`;
+
+   let screenSize = this.document.getElementById("ScreenSize");
+   screenSize.textContent = `Screen size is = Width: ${event.target.screen.width}, Height: ${event.target.screen.height}`;
+}11)
