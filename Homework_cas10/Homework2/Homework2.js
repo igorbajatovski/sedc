072e2db3686ceaadf35197d822d85e4392cce9a3
igorbajatovski <igorbajatovski@gmail.com>
2019-01-10 06:29:39 +0100

    Final Commit

--- Homework_cas10/Homework2/Homework2.js
+++ Homework_cas10/Homework2/Homework2.js
@@ -2,6 +2,30 @@
 
         () => {
 
+            ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Array', 'Object'].forEach( 
+                function(name) { 
+                    window['is' + name] = function(obj) {
+                        return Object.prototype.toString.call(obj) == '[object ' + name + ']';
+                }; 
+            });
+            
+            let s = "";
+            console.log(s instanceof String ? "E String" : "Ne e String");
+            console.log(s instanceof Object ? "E Object" : "Ne e Object");
+            console.log(typeof s);
+            console.log(Object.prototype.toString.call(s));
+            s = [];
+            console.log(s instanceof Array ? "E Array" : "Ne e Array");
+            s = 5;
+            console.log(typeof s);
+            s = Number(5);
+            console.log(Object.prototype.toString.call(s));
+            console.log(typeof s);
+            s = [];
+            console.log(typeof s);
+            console.log(typeof printMe);
+            console.log(Object.prototype.toString.call(s));
+
             function printMe(me)
             {
                 body = $("body");
@@ -9,14 +33,27 @@
                 
                 for (const prop in me) {
                     let li = $("<li>");
-                    li.text(me[prop]);
                     ul.append(li);
+
+                    if( !isObject(me[prop]) )
+                        li.text(me[prop]);
+                    else{
+                        sObject = ``;
+                        for (const key in me[prop]) {
+                            if (me[prop].hasOwnProperty(key)) {
+                                const element = me[prop][key];
+                                sObject += `${element}, `
+                            }
+                        }
+                        sObject = sObject.substring(0, sObject.length - 2);
+                        li.text(sObject);
+                    }
+
                 }
 
                 body.append(ul);
             }
-
-
+            
             $.ajax(
                 {
                     url: "https://igorbajatovski.github.io/sedc/Homework_cas10/Homework2/me.json",
