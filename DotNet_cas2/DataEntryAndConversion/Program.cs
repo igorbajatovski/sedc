2c0c8b761c5d89d1efaa7a036ee6800295a303b3
Igor Bajatovski <igorbajatovski@gmail.com>
2019-02-26 19:53:48 +0100

    Commit

---
+++ DotNet_cas2/DataEntryAndConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEntryAndConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some number:");
+            //var result = int.Parse(Console.ReadLine());
+            //string input1 = Console.ReadLine();
+            //int parsedResult = int.Parse(input1);
+            //int convertedResult = Convert.ToInt32(input1);
+            int tryParsedResult;
+            if(int.TryParse(input1, out tryParsedResult))
+                Console.WriteLine("The value " + tryParsedResult + " is of type: " + tryParsedResult.GetType());
+            else
+                Console.WriteLine("Not a number entered.");
+            Console.ReadLine();
+        }
+    }
+}
