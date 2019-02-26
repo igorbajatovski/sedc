845ac0f4d9bf683f60e5f4f518117732f7067bf3
Igor Bajatovski <igorbajatovski@gmail.com>
2019-02-26 18:28:24 +0100

    Comit

---
+++ DotNet_cas2/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerVariable1 = 5;
+            int integerVariable2;
+            integerVariable2 = -100;
+            integerVariable2 = 12;
+            float floatVariable = 2.5f;
+            double doubleVariable = 2.5;
+            string stringVariable = "Hello SEDC!";
+            char characterVariable = '^';
+            bool boolVariable = true;
+            long longerInteger = 5465465435321321324;
+            Console.WriteLine(integerVariable1);
+            Console.WriteLine(integerVariable2);
+            Console.ReadLine();
+        }
+    }
+}
