0032dfbbf6bf9e9b96f4fb9af82a31898e4ad247
Igor Bajatovski <igorbajatovski@gmail.com>
2019-02-28 21:07:36 +0100

    Exercise6

---
+++ DotNet_cas3/Exercise6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[1];
+            string line = "";
+            do
+            {
+                Console.Write("Enter name: ");
+                line = Console.ReadLine();
+                if (line != "Y")
+                {
+                    names[names.Length - 1] = line;
+                    Array.Resize(ref names, names.Length + 1);
+                }
+                else
+                {
+                    Array.Resize(ref names, names.Length - 1);
+                }
+            } while (line != "Y");
+
+            for(int i = 0; i < names.Length; ++i)
+            {
+                if(i < names.Length - 1)
+                    Console.Write(names[i] + ",");
+                else
+                    Console.Write(names[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
