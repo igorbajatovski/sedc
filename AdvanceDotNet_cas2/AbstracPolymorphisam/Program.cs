1433c4e9299c0a7fcf9a617b5971302206c106c5
Igor Bajatovski <igorbajatovski@gmail.com>
2019-03-28 19:36:02 +0100

    Commit

---
+++ AdvanceDotNet_cas2/AbstracPolymorphisam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstracPolymorphisam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Console.WriteLine(dog.Describe());
+            Console.WriteLine(dog.Features());
+
+
+            Bird bird = new Bird();
+            Console.WriteLine(bird.Describe());
+            Console.WriteLine(bird.Features());
+
+            Console.ReadLine();
+        }
+    }
+}
