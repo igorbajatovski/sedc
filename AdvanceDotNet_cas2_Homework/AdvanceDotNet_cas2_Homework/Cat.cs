5fe6ea744bfe9abf3d419bdb79e9e09eb1f0a34b
igorbajatovski <igorbajatovski@gmail.com>
2019-03-30 08:33:07 +0100

    Commit homework

---
+++ AdvanceDotNet_cas2_Homework/AdvanceDotNet_cas2_Homework/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceDotNet_cas2_Homework
+{
+    class Cat : IPredator, IPray
+    {
+        public int AttackSpeed { get; set; }
+        public string Name { get; set; }
+        public int FleeSpeed { get; set; }
+
+        public void Attack(IPray pray)
+        {
+            if (this.AttackSpeed > pray.FleeSpeed)
+                Console.WriteLine(this.Name + " catches " + pray.Name);
+            else
+                Console.WriteLine(pray.Name + " escapes from " + this.Name);
+        }
+
+        public void Flee()
+        {
+            Console.WriteLine(this.Name + " flees away...");
+        }
+    }
+}
---
+++ AdvanceDotNet_cas2_Homework/AdvanceDotNet_cas2_Homework/Gazella.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceDotNet_cas2_Homework
+{
+    class Gazella : IPray
+    {
+        public int FleeSpeed { get; set; }
+        public string Name { get; set; }
+
+        public void Flee()
+        {
+            Console.WriteLine(this.Name + " flees away...");
+        }
+    }
+}
