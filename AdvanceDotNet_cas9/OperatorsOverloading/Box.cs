1867ce9c920b4713c00d1dafbea91ce37c3e2d29
Igor Bajatovski <igorbajatovski@gmail.com>
2019-04-16 19:28:27 +0200

    Commit class 9

---
+++ AdvanceDotNet_cas9/OperatorsOverloading/Box.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloading
+{
+    public class Box
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Length { get; set; }
+
+        public Box() { }
+
+        public Box(int width, int height, int length)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Length = length;
+        }
+
+        public int Volume()
+        {
+            return Width * Height * Length;
+        }
+
+        public int Area()
+        {
+            return Width * Length * 2 + Width * Height * 2 + Height * Length * 2;
+        }
+
+        public static Box operator +(Box box1, Box box2)
+        {
+            return new Box(box1.Width + box2.Width, box1.Height + box2.Height, box1.Length + box2.Length);
+        }
+
+        public static Box operator -(Box box1, Box box2)
+        {
+            return new Box(box1.Width - box2.Width, box1.Height - box2.Height, box1.Length - box2.Length);
+        }
+        static int i;
+        public static Box operator ++(Box a)
+        {
+            Console.WriteLine("i = {0}", ++i);
+            return new Box(a.Width + 1, a.Height + 1, a.Length + 1);
+        }
+
+        public static Box operator --(Box a)
+        {
+            return new Box(a.Width - 1, a.Height - 1, a.Length - 1);
+        }
+
+        public static bool operator ==(Box box1, Box box2)
+        {
+            return box1.Width == box2.Width && box1.Height == box2.Height && box1.Length == box2.Length;
+        }
+
+        public static bool operator !=(Box box1, Box box2)
+        {
+            return box1.Width != box2.Width || box1.Height != box2.Height || box1.Length != box2.Length;
+        }
+
+        public static bool operator !(Box box)
+        {
+            return (box.Width == 0 && box.Height == 0 && box.Length == 0);
+        }
+
+        public static bool operator &(Box box1, Box box2)
+        {
+            bool flag1 = (box1.Width == 0 && box1.Height == 0 && box1.Length == 0);
+            bool flag2 = (box2.Width == 0 && box2.Height == 0 && box2.Length == 0);
+
+            return flag1 & flag2;
+        }
+
+        public static bool operator |(Box box1, Box box2)
+        {
+            bool flag1 = (box1.Width == 0 && box1.Height == 0 && box1.Length == 0);
+            bool flag2 = (box2.Width == 0 && box2.Height == 0 && box2.Length == 0);
+
+            return flag1 | flag2;
+        }
+
+
+    }
+}
+
---
+++ AdvanceDotNet_cas9/OperatorsOverloading/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsOverloading
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(1,1,1);
+            Console.WriteLine("Box1 Volume: {0}", box1.Volume());
+            Console.WriteLine("Box1 Area: {0}", box1.Area());
+
+            Box box2 = new Box(1, 2, 3);
+            Console.WriteLine("Box2 Volume: {0}", box2.Volume());
+            Console.WriteLine("Box2 Area: {0}", box2.Area());
+
+            Box box3;
+            box3 = box1 + box2;
+            Console.WriteLine("Box3 Volume: {0}", box3.Volume());
+            Console.WriteLine("Box3 Area: {0}", box3.Area());
+
+            Box box4 = box2 - box1;
+            Console.WriteLine("Box4 Volume: {0}", box4.Volume());
+            Console.WriteLine("Box4 Area: {0}", box4.Area());
+
+            Box box5 = box1++;
+            Console.WriteLine("Box5 Volume: {0}", box5.Volume());
+            Console.WriteLine("Box5 Area: {0}", box5.Area());
+
+            Box box6 = --box3;
+            Console.WriteLine("Box6 Volume: {0}", box6.Volume());
+            Console.WriteLine("Box6 Area: {0}", box6.Area());
+
+            if(box1 == box3)
+                Console.WriteLine("Box1 and Box3 are same");
+            else
+                Console.WriteLine("Box1 and Box3 are different");
+
+            Box box7 = new Box(1, 1, 1);
+
+            if (--box1 == box5)
+                Console.WriteLine("Box1 and Box5 are same");
+            else
+                Console.WriteLine("Box1 and Box5 are different");
+
+            if (box1.Equals(box5))
+                Console.WriteLine("Box1 and Box5 are same by ref");
+            else
+                Console.WriteLine("Box1 and Box5 are different by ref");
+
+            bool b = false;
+            if(!b)
+            {
+                Console.WriteLine("true");
+            }
+
+            Box box8 = new Box(0, 0, 0);
+            if(!box8)
+            {
+                Console.WriteLine("Not a box");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
