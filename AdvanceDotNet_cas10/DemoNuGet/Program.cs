4aa209816a03969d27bcf347e88e358b920c6b73
Igor Bajatovski <igorbajatovski@gmail.com>
2019-04-18 20:44:00 +0200

    Commit class 10

--- AdvanceDotNet_cas10/DemoNuGet/Program.cs
+++ AdvanceDotNet_cas10/DemoNuGet/Program.cs
@@ -9,10 +9,18 @@ using System.IO;
 
 namespace DemoNuGet_NewtonSoftJson
 {
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             JObject o = new JObject()
             {
                 new JProperty("Name", "Igor"),
@@ -40,8 +48,22 @@ namespace DemoNuGet_NewtonSoftJson
             string[] firmi = new string[] { "Seavus", "SEDC" };
             o.Add("Firms", JToken.FromObject(firmi));
             Console.WriteLine(o.ToString());
+            */
+            List<JObject> jCustomers = new List<JObject>();
+            Customer c1 = new Customer() { ID = 1, Name = "Igor", City = "Skopje" };
+            Customer c2 = new Customer() { ID = 2, Name = "Martin", City = "Skopje" };
+            Customer c3 = new Customer() { ID = 3, Name = "Dejan", City = "Stip" };
+            JObject jC1 = JObject.FromObject(c1);
+            JObject jC2 = JObject.FromObject(c2);
+            JObject jC3 = JObject.FromObject(c3);
+            jCustomers.Add(jC1);
+            jCustomers.Add(jC2);
+            jCustomers.Add(jC3);
 
+            Console.WriteLine(string.Join("\n", jCustomers));
 
+            string json = JsonConvert.SerializeObject(jCustomers);
+            Console.WriteLine("jCustomers: " + json);
         }
     }
 }
