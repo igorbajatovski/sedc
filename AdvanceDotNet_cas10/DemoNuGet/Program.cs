1599fc2246b4a59b8353ac7d8f896c945d7b0b3e
Igor Bajatovski <igorbajatovski@gmail.com>
2019-04-18 20:32:25 +0200

    Commit class 10

--- AdvanceDotNet_cas10/DemoNuGet/Program.cs
+++ AdvanceDotNet_cas10/DemoNuGet/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace DemoNuGet_NewtonSoftJson
 {
@@ -11,6 +13,34 @@ namespace DemoNuGet_NewtonSoftJson
     {
         static void Main(string[] args)
         {
+            JObject o = new JObject()
+            {
+                new JProperty("Name", "Igor"),
+                new JProperty("Age", 35),
+                new JProperty("City", "Skopje")
+            };
+
+            Console.WriteLine(o.ToString());
+            string path = @"C:\Users\igor.bajatovski\Documents\IBA\repo\AdvanceDotNet_cas10\DemoNuGet\MyFirstJson.json";
+            //File.WriteAllText(path, o.ToString());
+
+            JObject jObject;
+            jObject = JObject.Parse(File.ReadAllText(path));
+            Console.WriteLine(jObject);
+
+            List<string> skills = new List<string>()
+            {
+                "C#", "Sql Server"
+            };
+
+            o["Skills"] = JToken.FromObject(skills);
+
+            Console.WriteLine(o.ToString());
+
+            string[] firmi = new string[] { "Seavus", "SEDC" };
+            o.Add("Firms", JToken.FromObject(firmi));
+            Console.WriteLine(o.ToString());
+
 
         }
     }
