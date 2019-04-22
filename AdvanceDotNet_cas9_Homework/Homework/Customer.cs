382ec2784ef19ea83f2ad149ef17858fc2480747
igorbajatovski <igorbajatovski@gmail.com>
2019-04-23 00:19:33 +0200

    AdvancedDotNet Homework class 9

---
+++ AdvanceDotNet_cas9_Homework/Homework/Customer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Customer : IComparable<Customer>, IEquatable<Customer>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int MonthlySpend { get; set; }
+
+        public Customer() { }
+        public Customer(string name, string address, string city, int monthlySpend)
+        {
+            this.Name = name; this.Address = address; this.City = city; this.MonthlySpend = monthlySpend;
+        }
+        public void PrintCustomer()
+        {
+            Console.WriteLine($"Customer: {Name}, {Address}, {City}, {MonthlySpend}");
+        }
+        public int CompareTo(Customer c)
+        {
+            return this.MonthlySpend.CompareTo(c.MonthlySpend);
+        }
+
+        public bool Equals(Customer other)
+        {
+            if (this.Name == other.Name && this.Address == other.Address && this.City == other.City)
+                return true;
+            return false;
+        }
+
+        public static bool operator ==(Customer c1, Customer c2)
+        {
+            if (c1.Name == c2.Name && c1.Address == c2.Address && c1.City == c2.City)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Customer c1, Customer c2)
+        {
+            if (c1.Name != c2.Name || c1.Address != c2.Address || c1.City != c2.City)
+                return true;
+            return false;
+        }
+    }
+
+    public class CustomerMonthlySpendComparer : IComparer<Customer>
+    {
+        public int Compare(Customer c1, Customer c2)
+        {
+            return c1.MonthlySpend - c2.MonthlySpend;
+        }
+
+
+    }
+    public class CustomerCityComparer : IComparer<Customer>
+    {
+        public int Compare(Customer c1, Customer c2)
+        {
+            return string.Compare(c1.City, c2.City);
+        }
+    }
+}
---
+++ AdvanceDotNet_cas9_Homework/Homework/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("igor", "partizanska 95", "Skopje", 30000);
+            Customer c2 = new Customer("dejan", "roza luxemburg 1", "Skopje", 25000);
+            Customer c3 = new Customer("ivan", "partizanska 95", "Strumica", 29000);
+            Customer c4 = new Customer("petar", "vodjanska 95", "Bitola", 35000);
+            Customer c5 = new Customer("marko", "helsinshka", "Veles", 21000);
+            Customer c6 = new Customer("igor", "partizanska 95", "Skopje", 21000);
+
+            Customer[] customers = new Customer[] { c1, c2, c3, c4, c5 };
+            Array.Sort(customers, new CustomerMonthlySpendComparer());
+            foreach (var c in customers)
+            {
+                c.PrintCustomer();
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            Array.Sort(customers);
+            foreach (var c in customers)
+            {
+                c.PrintCustomer();
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            Array.Sort(customers, new CustomerCityComparer());
+            foreach (var c in customers)
+            {
+                c.PrintCustomer();
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            // Excercise 1
+            Console.WriteLine("Equality c1 and c5: {0}", c1.Equals(c5));
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            if (c1 == c5)
+                Console.WriteLine("c1 == c5");
+            else
+                Console.WriteLine("c1 != c5");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            if (c1 == c6)
+                Console.WriteLine("c1 == c6");
+            else
+                Console.WriteLine("c1 != c6");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        }
+    }
+}
