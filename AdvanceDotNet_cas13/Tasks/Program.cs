beebdf62b5c62d562380e146b29862cd257b1a34
Igor Bajatovski <igorbajatovski@gmail.com>
2019-05-02 20:04:32 +0200

    Commit class 13

---
+++ AdvanceDotNet_cas13/Tasks/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Task myTask = new Task( () =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Hello there, after 2000ms");
+            });
+
+            myTask.Start();
+
+            Task<string> myName = new Task<string>(() =>
+               {
+                   Thread.Sleep(5000);
+                   Console.WriteLine("Hello from SEDC!");
+                   return "Hello my name is Igor";
+               });
+            myName.Start();
+            Console.WriteLine(myName.Result);
+            */
+
+            /*
+            Task<int> myIntTask = new Task<int>( () =>
+            {
+                Thread.Sleep(3000);
+                return 6;
+            });
+            myIntTask.Start();
+
+            Task.Run(() =>
+           {
+               Thread.Sleep(3000);
+               Console.WriteLine("This is executed immediately!");
+           });
+           */
+
+            for(int i = 0; i < 20; ++i)
+            {
+                int temp = i;
+                Task.Run( () =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Task No. {0}", temp);
+                });
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdvanceDotNet_cas13/Threads/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SendMessage();
+            SendMessagesWithThreads();
+        }
+
+        public static void SendMessage()
+        {
+            Console.WriteLine("Getting Ready...");
+            Thread.Sleep(2000);
+            Console.WriteLine("First message arrived!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Second message arrived!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Third message arrived!");
+            Console.WriteLine("The app has finished!");
+            Console.ReadLine();
+        }
+
+        public static void SendMessagesWithThreads()
+        {
+            Console.WriteLine("Geting started...");
+
+            Thread myThread = new Thread( () =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("First message arrived!");
+            });
+            myThread.Start();
+            
+
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Second message arrived!");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Third message arrived!");
+            }).Start();
+
+            Console.WriteLine("All messages have been received");
+            Console.ReadLine();
+        }
+    }
+
+}
