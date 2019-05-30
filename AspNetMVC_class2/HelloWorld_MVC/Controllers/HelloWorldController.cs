09184712a868cdaa6f0799ce7f7ad502d385cf12
Igor Bajatovski <igorbajatovski@gmail.com>
2019-05-30 20:57:26 +0200

    Class 2

---
+++ AspNetMVC_class2/HelloWorld_MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld_MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
