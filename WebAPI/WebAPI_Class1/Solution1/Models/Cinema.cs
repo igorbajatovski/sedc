9471eb0d884f46af8ef2bd1ed752876f2e878779
Igor Bajatovski <igorbajatovski@gmail.com>
2019-08-20 17:50:20 +0200

    Commit

---
+++ WebAPI/WebAPI_Class1/Solution1/Models/Cinema.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Cinema()
+        {
+        }
+
+        public Cinema(int id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+    }
+}
--- WebAPI/WebAPI_Class1/Solution1/WebAppDemo/Startup.cs
+++ WebAPI/WebAPI_Class1/Solution1/WebAppDemo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,12 @@ namespace WebAppDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(c =>
+            {
+                c.RespectBrowserAcceptHeader = true;
+                c.InputFormatters.Add(new XmlSerializerInputFormatter(new MvcOptions()));
+                c.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
